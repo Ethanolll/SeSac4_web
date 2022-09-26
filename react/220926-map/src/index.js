c682df0089501026cc830331278a3cd7159b2ebb
Ethanolll <icedgreentea676@gmail.com>
2022-09-26 18:41:45 +0900

    study: react 220926

--- react/220926-map/src/index.js
+++ react/220926-map/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import ComponentTest from './ComponentTest';
+import ReactTest1 from './ReactTest1';
 // import Event_ex from './Event_ex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <ComponentTest/>
+    <ReactTest1/>
     {/* <Event_ex/> */}
   </React.StrictMode>
 );
