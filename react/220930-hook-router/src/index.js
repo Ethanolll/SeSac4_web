012c6591d4494f4d9e838f6ed1e914a0a1988320
Ethanolll <icedgreentea676@gmail.com>
2022-09-30 16:32:51 +0900

    study: scss

--- react/220930-hook-router/src/index.js
+++ react/220930-hook-router/src/index.js
@@ -5,6 +5,10 @@ import Reducer from "./2HooksReducer";
 import Memo from "./3HooksMemo";
 import Callback from "./4HooksCallback";
 import Ref from './5HookRef';
+import Origin from './6Origin';
+import UseSass from './7UseSass';
+import CSSModule from './8CSSModule';
+import StyledComponents from './9StyledComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,5 +22,13 @@ root.render(
     <Callback/>
     <hr/>
     <Ref/>
+    <hr/>
+    <Origin/>
+    <hr/>
+    <UseSass/>
+    <hr/>
+    <CSSModule/>
+    <hr/>
+    <StyledComponents/>
   </React.StrictMode>
 );
